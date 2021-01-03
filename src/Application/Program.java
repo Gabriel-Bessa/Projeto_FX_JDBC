6b2a3cfc68239e70977853bfa51f1b67a8706438
Gabriel Bessa <bessagabriel490@gmail.com>
2021-01-03 00:11:36 -0300

    Design da MainView!

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -3,27 +3,31 @@ package Application;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Program extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
         try {
-            Parent parent = FXMLLoader.load(getClass().getResource("/gui/MainView.fxml"));
-            Scene scene = new Scene(parent);
-            stage.setScene(scene);
-            stage.setTitle("Execução");
-            stage.show();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            ScrollPane scrollPane = loader.load();
+
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            Scene mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
 }
